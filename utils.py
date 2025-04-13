864ff9d48561f4819c0e118cf9297b332b928824
PRIME BOT'z <sahidhasannayem@gmail.com>
2025-04-13 11:48:33 +0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ import logging, asyncio, os, re, random, pytz, aiohttp, requests, string, json, 
 from info import *
 from imdb import Cinemagoer 
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
-from pyrogram import enums
+from pyrogram import Client, enums
 from pyrogram.errors import *
 from typing import Union
 from Script import script
