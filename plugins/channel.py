951c310734f15800cedcfeeb359c06400eee2ab7
PRIME BOT'z <sahidhasannayem@gmail.com>
2025-04-13 10:10:29 +0600

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -25,7 +25,7 @@ async def media(bot, message):
         return
     media.file_type = file_type
     media.caption = message.caption
-    success, result = await save_file(bot, media)
+    success, result = await save_file(media)
     try:
         if success and result == 1 and await get_status(bot.me.id):
             await send_movie_update(bot, file_name=media.file_name, caption=media.caption)
