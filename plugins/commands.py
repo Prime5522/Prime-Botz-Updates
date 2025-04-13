d42bc367e1b02099ff68029b5ad6074f9d724b42
PRIME BOT'z <sahidhasannayem@gmail.com>
2025-04-13 07:08:02 +0600

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -143,7 +143,7 @@ async def start(client, message):
         searches = message.command[1].split("-", 1)[1] 
         search = searches.replace('-', ' ')
         message.text = search 
-        await auto_filter(client, message) 
+        await auto_filter(client, msg=message)
         return
                     
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
